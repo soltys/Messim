ceb5adf57bec37b4cb6acb2ccccbb9f11725be4e
Paweł Sołtysiak <soltys@uznam.org>
2012-02-23 20:37:02 +0100

    added custom MessimMembershipProvider and MessimRoleProvider

---
+++ Messim.UI/Authentication/MessimMembershipProvider.cs
@@ -0,0 +1,158 @@
+﻿using System.Linq;
+using System.Web.Security;
+using Messim.UI.Models;
+using Messim.Util;
+
+namespace Messim.UI.Authentication
+{
+    class MessimMembershipProvider : MembershipProvider
+    {
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            status = MembershipCreateStatus.Success;
+            return null;
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            if (string.IsNullOrEmpty(password.Trim())) return false;
+            string hash = SHA.CreateSHA1Hash(password);
+            using (var _db = new MessimContext())
+            {
+                User user = _db.Users.SingleOrDefault(x => x.Username == username);
+                if (user == null) return false;
+                if (user.Password == hash)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override string ApplicationName
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ Messim.UI/Authentication/MessimRoleProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace Messim.UI.Authentication
+{
+    public class MessimRoleProvider : System.Web.Security.RoleProvider
+    {
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            if (username == "admin")
+            {
+                return true;
+            }
+
+            if (roleName != "regular")
+                return false;
+            return true;
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            return new[] { "regular" };
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
