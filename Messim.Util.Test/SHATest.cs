6b83089449e5f40aaa0d51fe298f55194208bda1
Paweł Sołtysiak <soltys@uznam.org>
2012-02-23 19:53:22 +0100

    Added new project named Messim.Util with class SHA which generates SHA1 SHA256 and SHA512 strings

---
+++ Messim.Util.Test/SHATest.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace Messim.Util.Test
+{
+    [TestFixture]
+    class SHATest
+    {
+        [Test]
+        public void SHA1_hash_is_equal_to_other_generator_using_default_encoding()
+        {
+            string wordToHash = "milk";
+            string milkHash = "cf5dbf0ec57dff5da56d86c45b3bdd11849a065a".ToUpperInvariant();
+
+            Assert.AreEqual(milkHash, SHA.CreateSHA1Hash(wordToHash));
+        }
+
+        [Test]
+        public void SHA512_hash_is_equal_to_other_generator_using_default_encoding()
+        {
+            string wordToHash = "milk";
+            string milkHash = "7586A2A3DA04B9C2610906E936D0365EAA601AD8CDF26D30501AFA2A27A3D42AB0023912B488F408D457BD958EBB24140C2BA029CE93A9702D9197A6E30C331D";
+
+            Assert.AreEqual(milkHash, SHA.CreateSHA512Hash(wordToHash));
+        }
+
+
+        [Test]
+        public void SHA512_hash_coverted_to_base64_still_represent_same_data()
+        {
+            string wordToHash = "milk";
+            string milkHash = "dYaio9oEucJhCQbpNtA2XqpgGtjN8m0wUBr6Kiej1CqwAjkStIj0CNRXvZWOuyQUDCugKc6TqXAtkZem4wwzHQ==";
+
+            string generatedHash = SHA.CreateSHA512HashToBase64String(wordToHash, Encoding.UTF8);
+
+
+            Assert.AreEqual(milkHash, generatedHash);
+        }
+    }
+}
---
+++ Messim.Util/SHA.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Messim.Util
+{
+    /// <summary>
+    /// Static class with methods to create SHA hash strings
+    /// </summary>
+    public static class SHA
+    {
+        /// <summary>
+        /// Creates SHA-1 for text
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <param name="encoding">Encoding for text</param>
+        /// <returns>Cryptografic hash SHA-1</returns>
+        public static string CreateSHA1Hash(string text, Encoding encoding)
+        {
+            byte[] buffer = encoding.GetBytes(text);
+            var cryptoTransformSHA1 =
+            new SHA1CryptoServiceProvider();
+            string hash = BitConverter.ToString(
+                cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+
+        /// <summary>
+        /// Creates SHA-1 for text, encoding for text is Encoding.Default
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <returns>Cryptografic hash SHA-1</returns>
+        public static string CreateSHA1Hash(string text)
+        {
+            return CreateSHA1Hash(text, Encoding.Default);
+        }
+
+        /// <summary>
+        /// Creates SHA-512 for text
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <param name="encoding">Encoding for text</param>
+        /// <returns>Cryptografic hash SHA-512</returns>
+        public static string CreateSHA512Hash(string text, Encoding encoding)
+        {
+            byte[] buffer = encoding.GetBytes(text);
+            var cryptoTransformSHA512 = new SHA512CryptoServiceProvider();
+
+            string hash = BitConverter.ToString(
+                cryptoTransformSHA512.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+
+        public static string CreateSHA512HashToBase64String(string text, Encoding encoding)
+        {
+            byte[] buffer = encoding.GetBytes(text);
+            var cryptoTransformSHA512 = new SHA512CryptoServiceProvider();
+
+            string hash = Convert.ToBase64String(
+                cryptoTransformSHA512.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+        /// <summary>
+        /// Creates SHA-512 for text, encoding for text is Encoding.Default
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <returns>Cryptografic hash SHA-512</returns>
+        public static string CreateSHA512Hash(string text)
+        {
+            return CreateSHA512Hash(text, Encoding.Default);
+        }
+
+        /// <summary>
+        /// Creates SHA-256 for text
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <param name="encoding">Encoding for text</param>
+        /// <returns>Cryptografic hash SHA-256</returns>
+        public static string CreateSHA256Hash(string text, Encoding encoding)
+        {
+            byte[] buffer = encoding.GetBytes(text);
+            var cryptoTransformSHA512 = new SHA256CryptoServiceProvider();
+
+            string hash = BitConverter.ToString(
+                cryptoTransformSHA512.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+
+        /// <summary>
+        /// Creates SHA-256 for text, encoding for text is Encoding.Default
+        /// </summary>
+        /// <param name="text">Input text from which is created SHA1</param>
+        /// <returns>Cryptografic hash SHA-256</returns>
+        public static string CreateSHA256Hash(string text)
+        {
+            return CreateSHA256Hash(text, Encoding.Default);
+        }
+
+    }
+}
